36a7946ec26c680fc67fd998fed273d38ac69600
Antonello Luppolo <57115680+hellswipe@users.noreply.github.com>
2022-06-24 18:53:15 +0200

    Create correctness_verifier.sh

---
+++ correctness_verifier.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+nBodies=$1 #prende in input come primo parametro il numero di bodies 
+n_proc=$2  #prende in input come secondo parametro il numero di massimo di n processi
+#controlla se viene specificato il parametro -t per eseguire il programma che salva i risultati su file
+if [[ "$#" -eq 3 && $3 == "-t" ]] 
+then
+    mpirun -np 1 nbody_def.out $nBodies -t
+fi
+#per non mostare l'output del programma, ma solo l'output dei test, specificare -dn
+if [[ "$#" -eq 4 && $4 == "-dn" ]] 
+then
+    mpirun -np 1 nbody_def.out $nBodies -t 1>/dev/null
+fi
+# attendiamo che il programma sequenziale termini la sua esecuzione
+wait
+printf "sequential program ready\n\n"
+# settiamo la variabile proc la quale va a specificare il numero di processi per l'i-esima esecuzione 
+proc=2
+# testiamo la correttezza del programma eseguendo lo stesso con un numero progressivo di processi 
+# cioè eseguiamo il programma con 1,2,3,...,max_nproc 
+while [[ ((proc -le n_proc)) ]]
+do  
+	if [ "$#" -eq 4 ] 
+	then
+	    mpirun -np $proc nbody_def.out $nBodies -t 1>/dev/null
+    	else
+	    mpirun -np $proc nbody_def.out $nBodies -t
+	fi
+	# attendiamo che il programma termini la sua esecuzione 
+	wait
+	# confrontiamo il risultati che ha generato e memorizzato nel suo corrispondente file con quelli
+	# generati dal file sequenziale tramite il comando diff nel quale specifichiamo il parametro -q
+  # che indica a diff di fornire l'output solo se ci sono differenze tra i due file
+	DIFF=$(diff -q parallel_$proc sequential_program )
+	if [ "$DIFF" != "" ] 
+	then
+	    printf "parallel program np %d \!\!\! ERRORE \!\!\! \n\n" $proc
+		break
+	else
+	    printf "parallel program np %d --> OK\n\n" $proc
+	fi
+((proc++))
+done  
